b3b7f869082d459ba2147ae65e267a4077e7b445
juan <residenciaunach@gmail.com>
2026-02-18 08:49:47 -0600

    Permitir imagenes desde el backend de Render

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'backend-eccomerce-615t.onrender.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
